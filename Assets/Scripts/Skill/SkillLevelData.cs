6e5040b66ff62afbb588756257ed1fb9cded6bed
Yun <1668430002@qq.com>
2026-03-12 09:48:29 +0800

    remove unused resource

--- Assets/Scripts/Skill/SkillLevelData.cs
+++ Assets/Scripts/Skill/SkillLevelData.cs
@@ -4,6 +4,6 @@ public class SkillLevelData
 {
     public int skillLevel;
     //TODO这个应该新建对应的SkillType枚举类型
-    public int skillType;
+    public SkillType skillType;
     public SkillScaleData skillScaleData;
 }
